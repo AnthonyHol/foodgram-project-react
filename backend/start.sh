41eb1f4aeb1b7d746f86a730026832b39811be6c
Anthony <1tonyhol1@gmail.com>
2022-12-20 09:30:19 +0300

    Update database to PostgreSQL
    It works!

--- backend/start.sh
+++ backend/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic --no-input
-python manage.py import_data
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py collectstatic --no-input
+python3 manage.py import_data
 gunicorn foodgram.wsgi:application --bind 0:8000
