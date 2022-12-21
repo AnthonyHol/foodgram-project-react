9e14a8dddada5c41fd613fb5c0bbeee0ff584931
Anthony <1tonyhol1@gmail.com>
2022-12-21 16:08:54 +0300

    Update settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-MEDIA_URL = "http://localhost/media/"
+MEDIA_URL = "http://158.160.10.39:3001/media/"
 # MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
